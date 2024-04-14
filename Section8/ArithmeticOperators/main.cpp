138a3ec8a18404879b41fd9ce71167e56298c521
Ryan <rcbarke@gmail.com>
2024-04-14 18:24:58 -0400

    Section 8 50% commit

--- Section8/ArithmeticOperators/main.cpp
+++ Section8/ArithmeticOperators/main.cpp
@@ -19,7 +19,7 @@ int main() {
     int num1 {200};
     int num2 {100};
     
-  //  cout << num1 << " + " <<  num2 << " = "<< num1+ num2 << endl;
+    cout << num1 << " + " <<  num2 << " = "<< num1+ num2 << endl;
  
     int result {0};
     
--- Section8/AssignmentOperator/main.cpp
+++ Section8/AssignmentOperator/main.cpp
@@ -10,7 +10,7 @@ int main() {
     int num1 {10};
     int num2 {20};
     
-    num = 100;
+    num1 = 100;
     
     cout << "num1 is " << num1 << endl;
     cout << "num2 is " << num2 << endl;
